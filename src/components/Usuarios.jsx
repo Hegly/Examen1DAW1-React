0a3c106e757d22648b6ec2daf1b8f5b06c1f9abf
Hegly <clasesccnn1@gmail.com>
2024-03-01 22:17:46 -0600

    Formulario
    Creación de formulario

--- src/components/Usuarios.jsx
+++ src/components/Usuarios.jsx
@@ -1,58 +1,54 @@
 import React from 'react';
+import {useState} from 'react';
 
 export const Usuarios = () => {
 
-    const users = [
-        {
-            id: 1,
-            nombre: 'Usuario 1',
-            correo: 'usuario1@example.com',
-            contraseña: '1234'
-        },
-        {
-            id: 2,
-            nombre: 'Usuario 2',
-            correo: 'usuario2@example.com',
-            contraseña: '5678'
-        }, 
-        {
-            id: 3,
-            nombre: 'Usuario 3',
-            correo: 'usuario3@example.com',
-            contraseña: '1357'
-        }
-    ];
+    const [nombre, setNombre] = useState();
 
-  return (
+    const [correo, setCorreo] = useState();
+
+    const [contrasena, setContrasena] = useState();
+
+    const usuarioHandler = (event) =>{
+
+        console.log(event);
+
+    }
+
+    return (
     <>
-        <div className='container'>
+        <div className='container mt-5'>
+            <form>
+                <fieldset>
+                    <legend>Registro de Usuarios</legend>
 
-            <h1>Usuarios</h1>
-            
-                <table className="table table-success table-striped">
-                    <thead>
-                        <tr>
-                        <th scope="col" className="border border-secondary border-2 text-center">id</th>
-                        <th scope="col" className="border border-secondary border-2 text-center">Nombre</th>
-                        <th scope="col" className="border border-secondary border-2 text-center">Correo</th>
-                        <th scope="col" className="border border-secondary border-2 text-center">Contraseña</th>
-                        </tr>
-                    </thead>
-                    
-                    <tbody>
-                        {users.map( (user) =>(
-                            <tr key={user.id}> 
-                                <th scope="row" className="border border-secondary border-2 text-center"></th>
-                                <td className="border border-secondary border-2 text-center">{user.nombre}</td>
-                                <td className="border border-secondary border-2 text-center">{user.correo} </td>
-                                <td className="border border-secondary border-2 text-center">{user.contraseña} </td>
-                            </tr>
-                        ) )}
-                    </tbody>
+                    <div class="form-group row">
+                            <label for="staticNombre" class="col-sm-2 col-form-label">Nombre</label>
+                        <div class="col-sm-10">
+                            <input type="nombre" class="form-control" id="exampleInputNombre1" name="nombre" placeholder="Escriba el nombre del usuario"
+                            onChange={usuarioHandler}
+                            />
+                        </div>
+                        </div>
 
-                </table>
+                        <div class="form-group row mt-3">
+                            <label for="staticCorreo" class="col-sm-2 col-form-label">Correo</label>
+                        <div class="col-sm-10">
+                            <input type="correo" class="form-control" id="exampleInputCorreo1" name="correo" placeholder="Escriba el correo del usuario"/>
+                        </div>
+                        </div>
 
+                        <div class="form-group row mt-3">
+                            <label for="staticContrasena" class="col-sm-2 col-form-label">Contraseña</label>
+                        <div class="col-sm-10">
+                            <input type="contrasena" class="form-control" id="exampleInputContrasena1" name="contrasena" placeholder="Escriba la contraseña del usuario"/>
+                        </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary w-100">Enviar</button>
+                </fieldset>
+            </form>
         </div>
     </>
-  );
+    )
 }
